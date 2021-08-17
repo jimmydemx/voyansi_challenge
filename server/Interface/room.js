03decb3045299e10a41efb8dddb08fdd9088d511
jimmydemx <j_demx@yahoo.com>
2021-08-17 23:52:58 +0200

    edit room

--- server/Interface/room.js
+++ server/Interface/room.js
@@ -7,53 +7,53 @@ import axios from '../util/axios'
 let router= new Router()
 
 
-router.post('/room',async(ctx)=>{
-    var newRoom= await room.create({
-        id:1,
-        name:'room1',
-        occupant:"teacher",
-        numer:2
-    })
+// router.post('/room',async(ctx)=>{
+//     var newRoom= await room.create({
+//         id:1,
+//         name:'room1',
+//         occupant:"teacher",
+//         numer:2
+//     })
 
 
 
-})
+// })
 
-router.put('occupant',async(ctx)=>{
-
-
-
-})
+// router.put('occupant',async(ctx)=>{
 
 
 
+// })
 
 
-router.delete('/room',async(ctx)=>{
 
 
 
-})
-
-router.get("/room",async(ctx)=>{
+// router.delete('/room',async(ctx)=>{
 
 
-    console.log("12345")
-    ctx.set('Access-Control-Allow-Origin', '*');
 
-    var nuser= await room.create({
-        id:1,
-        name:'room1',
-        occupant:"teacher",
-        numer:2
-    })
-    ctx.body={
+// })
 
-        code:200,
-        data:nuser
-    }
+// router.get("/room",async(ctx)=>{
 
-})
+
+//     console.log("12345")
+//     ctx.set('Access-Control-Allow-Origin', '*');
+
+//     var nuser= await room.create({
+//         id:1,
+//         name:'room1',
+//         occupant:"teacher",
+//         numer:2
+//     })
+//     ctx.body={
+
+//         code:200,
+//         data:nuser
+//     }
+
+// })
 
 
 module.exports=router
