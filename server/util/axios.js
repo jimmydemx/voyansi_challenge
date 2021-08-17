43d51c0d32cc35d7f111e672ce90e013f83a14d5
jimmydemx <j_demx@yahoo.com>
2021-08-17 23:14:03 +0200

    update node

--- server/util/axios.js
+++ server/util/axios.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL="http://localhost:3000"
-
+axios.defaults.baseURL=`http://${process.env.HOST||'localhost'}:${process.env.PORT||3000}`
 
 
 export default axios
